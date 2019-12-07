b1086eec22e58284fd3791161988d0b3d1351dcd
Lydia Ahrens <lydiamidori@gmail.com>
2019-12-07 05:35:28 -0500

    more Demo

--- CleaningHouse-master/app/build/generated/not_namespaced_r_class_sources/debug/r/androidx/constraintlayout/widget/R.java
+++ CleaningHouse-master/app/build/generated/not_namespaced_r_class_sources/debug/r/androidx/constraintlayout/widget/R.java
@@ -74,19 +74,19 @@ public final class R {
         private id() {}
 
         public static final int bottom = 0x7f080026;
-        public static final int end = 0x7f08004c;
-        public static final int gone = 0x7f08005c;
-        public static final int invisible = 0x7f080068;
-        public static final int left = 0x7f08006d;
-        public static final int packed = 0x7f080088;
-        public static final int parent = 0x7f08008a;
-        public static final int percent = 0x7f08008e;
-        public static final int right = 0x7f080095;
-        public static final int spread = 0x7f0800b8;
-        public static final int spread_inside = 0x7f0800b9;
-        public static final int start = 0x7f0800be;
-        public static final int top = 0x7f0800d5;
-        public static final int wrap = 0x7f0800e9;
+        public static final int end = 0x7f08004d;
+        public static final int gone = 0x7f08005d;
+        public static final int invisible = 0x7f080069;
+        public static final int left = 0x7f08006e;
+        public static final int packed = 0x7f080089;
+        public static final int parent = 0x7f08008b;
+        public static final int percent = 0x7f08008f;
+        public static final int right = 0x7f080096;
+        public static final int spread = 0x7f0800b9;
+        public static final int spread_inside = 0x7f0800ba;
+        public static final int start = 0x7f0800bf;
+        public static final int top = 0x7f0800d7;
+        public static final int wrap = 0x7f0800eb;
     }
     public static final class styleable {
         private styleable() {}
--- CleaningHouse-master/app/build/generated/not_namespaced_r_class_sources/debug/r/com/google/firebase/R.java
+++ CleaningHouse-master/app/build/generated/not_namespaced_r_class_sources/debug/r/com/google/firebase/R.java
@@ -91,26 +91,26 @@ public final class R {
         public static final int blocking = 0x7f080025;
         public static final int cancel_action = 0x7f08002a;
         public static final int chronometer = 0x7f080030;
-        public static final int end_padder = 0x7f08004d;
-        public static final int forever = 0x7f080059;
-        public static final int icon = 0x7f080062;
-        public static final int icon_group = 0x7f080063;
-        public static final int info = 0x7f080067;
-        public static final int italic = 0x7f080069;
-        public static final int line1 = 0x7f08006f;
-        public static final int line3 = 0x7f080070;
-        public static final int media_actions = 0x7f080075;
-        public static final int normal = 0x7f080083;
-        public static final int notification_background = 0x7f080084;
-        public static final int notification_main_column = 0x7f080085;
-        public static final int notification_main_column_container = 0x7f080086;
-        public static final int right_icon = 0x7f080096;
-        public static final int right_side = 0x7f080097;
-        public static final int status_bar_latest_event_content = 0x7f0800bf;
-        public static final int text = 0x7f0800c7;
-        public static final int text2 = 0x7f0800c8;
-        public static final int time = 0x7f0800d0;
-        public static final int title = 0x7f0800d1;
+        public static final int end_padder = 0x7f08004e;
+        public static final int forever = 0x7f08005a;
+        public static final int icon = 0x7f080063;
+        public static final int icon_group = 0x7f080064;
+        public static final int info = 0x7f080068;
+        public static final int italic = 0x7f08006a;
+        public static final int line1 = 0x7f080070;
+        public static final int line3 = 0x7f080071;
+        public static final int media_actions = 0x7f080076;
+        public static final int normal = 0x7f080084;
+        public static final int notification_background = 0x7f080085;
+        public static final int notification_main_column = 0x7f080086;
+        public static final int notification_main_column_container = 0x7f080087;
+        public static final int right_icon = 0x7f080097;
+        public static final int right_side = 0x7f080098;
+        public static final int status_bar_latest_event_content = 0x7f0800c0;
+        public static final int text = 0x7f0800c9;
+        public static final int text2 = 0x7f0800ca;
+        public static final int time = 0x7f0800d2;
+        public static final int title = 0x7f0800d3;
     }
     public static final class integer {
         private integer() {}
