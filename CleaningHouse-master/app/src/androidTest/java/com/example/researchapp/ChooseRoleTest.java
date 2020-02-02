45ff05854b8526db64a456c68a9840e088ab1685
vicbot87 <vicbot87@gmail.com>
2020-02-02 18:11:45 -0500

    Create ChooseRoleTest.java

---
+++ CleaningHouse-master/app/src/androidTest/java/com/example/researchapp/ChooseRoleTest.java
@@ -0,0 +1,18 @@
+package com.example.researchapp;
+
+import org.junit.Test;
+import android.content.Intent;
+import static org.junit.Assert.*;
+import androidx.appcompat.app.AppCompatActivity;
+public class ChooseRoleTest {
+
+    @Test
+    public void onClick() {
+        ChooseRole activity = com.example.researchapp.ChooseRole;
+        activity.findViewById(R.id.hostBtn).performClick();
+        Intent expectedIntent = new Intent(this, SignUpScreen.class);
+       //attempting to compare nextPageHost()'s Intent to the expected intent
+        //not sure how to get the actual returned Intent to compare to the expected Intent
+        //may need to utilize Espresso-intents or Mockito 
+    }
+}
