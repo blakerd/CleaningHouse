4ced4bbe1c198cc296e49c919431503ee2d26f63
vicbot87 <vicbot87@gmail.com>
2020-02-25 14:01:58 -0500

    Upload New Property Button

--- CleaningHouse-master/app/src/main/java/com/example/researchapp/PropertiesScreen.java
+++ CleaningHouse-master/app/src/main/java/com/example/researchapp/PropertiesScreen.java
@@ -8,16 +8,18 @@ import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-
+import android.widget.Button;
 import android.view.View;
 
-public class PropertiesScreen extends AppCompatActivity {
+public class PropertiesScreen extends AppCompatActivity implements View.OnClickListener {
 
+    Button uploadNewPropertyButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_properties_screen);
         Toolbar toolbar = findViewById(R.id.toolbar);
+        uploadNewPropertyButton = (Button) findViewById(R.id.uploadNewPropertyButton);
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = findViewById(R.id.fab);
@@ -32,14 +34,14 @@ public class PropertiesScreen extends AppCompatActivity {
     @Override
     public void onClick(View v) {
         switch(v.getId()){
-            case R.id.cleanerBtn:
+            case R.id.uploadNewPropertyButton:
                 nextScreen();
                 break;
         }
     }
     private void nextScreen() {
 
-        Intent i = new Intent(this, CleanersHomeScreen.class);
+        Intent i = new Intent(this, UploadNewPropertyScreen.class);
         startActivity(i);
 
     }
