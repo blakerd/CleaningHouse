8be852794c39670c73cfb33c78e616755d75ff45
Tony Bonadio <fbonadio@email.sc.edu>
2020-03-02 00:11:23 -0500

    attempted TODO's for search

--- CleaningHouse-master/app/src/main/java/com/example/researchapp/Search.java
+++ CleaningHouse-master/app/src/main/java/com/example/researchapp/Search.java
@@ -1,42 +1,96 @@
 package com.example.researchapp;
 
-public class Search{
-    private List<Property> propertyList;
-    String input; //TODO Write method to take in user input for search via a button and textbox
 
-    //TODO add an "OnClick" reference to execute this code
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.database.*;
+
+import java.util.ArrayList;
 
 
-    propertyList = new ArrayList<>();
+public class Search extends AppCompatActivity implements View.OnClickListener{
+    ArrayList<Property> propertyList;
 
-    DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Properties");
+    String input;
 
-    //User query by city
-    Query cityQuery = reference.orderByChild("City").startAt(input).endAT(input +"\uf8ff");
-    cityQuery.addListenerForSingleValueEvent(valueEventListener);
+    String city;
+    String cleaningPrice;
+    String streetAddress;
 
-    //User query by price
-    Query priceQuery = reference.orderByChild("Cleaning Price").startAt(input).endAT(input +"\uf8ff");
-    priceQuery.addListenerForSingleValueEvent(valueEventListener);
+    EditText cityNameInput;
+    EditText cleaningPriceInput;
+    EditText streetAddressInput;
 
-    //User query by price
-    Query addressQuery = reference.orderByChild("Street Address").startAt(input).endAT(input +"\uf8ff");
-    addressQuery.addListenerForSingleValueEvent(valueEventListener);
+    Button searchButton;
 
-    ValueEventListener valueEventListener = new ValueEventListener(){
-        @Override
-        public void onDataChange(DataSnapshot dataSnapshot){
-            if(dataSnapshot.exists()){
-                for(DataSnapshot snapshot : dataSnapshot.getChildren()){
-                    Property property = snapshot.getValue(Property.class);
-                    propertyList.add(property);
-                }
-            }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+        //TODO Write method to take in user input for search via a button and textbox
+
+        //TODO add an "OnClick" reference to execute this code
+
+        cityNameInput = (EditText) findViewById(R.id.city);
+        cleaningPriceInput = (EditText) findViewById(R.id.cleaningPrice);
+        streetAddressInput = (EditText) findViewById(R.id.streetAddress);
+
+        searchButton = (Button) findViewById(R.id.searchButton);
+
+        searchButton.setOnClickListener(this);
+
+    }
+    public void onClick(View v) {
+        switch(v.getId()){
+
+            // case R.id.selectImageButton:
+            // uploadNewPropertyPage();
+            //  break;
+
+            case R.id.searchButton:
+                city = cityNameInput.getText().toString();
+                cleaningPrice = cleaningPriceInput.getText().toString();
+                streetAddress = streetAddressInput.getText().toString();
+                propertyList = new ArrayList<>();
+
+
+                ValueEventListener valueEventListener = new ValueEventListener(){
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot){
+                        if(dataSnapshot.exists()){
+                            for(DataSnapshot snapshot : dataSnapshot.getChildren()){
+                                Property property = snapshot.getValue(Property.class);
+                                propertyList.add(property);
+                            }
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                };
+                DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Properties");
+
+                //User query by city
+                Query cityQuery = reference.orderByChild("City").startAt(input).endAt(input +"\uf8ff");
+                cityQuery.addListenerForSingleValueEvent(valueEventListener);
+
+                //User query by price
+                Query priceQuery = reference.orderByChild("Cleaning Price").startAt(input).endAt(input +"\uf8ff");
+                priceQuery.addListenerForSingleValueEvent(valueEventListener);
+
+                //User query by price
+                Query addressQuery = reference.orderByChild("Street Address").startAt(input).endAt(input +"\uf8ff");
+                addressQuery.addListenerForSingleValueEvent(valueEventListener);
+                break;
         }
-
-        @Override
-        public void onCancelled(DatabaseError databaseError) {
-
-        }
-    };
+    }
 }
