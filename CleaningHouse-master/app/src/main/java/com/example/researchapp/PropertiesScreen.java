172adc2b6141c93ff4dda31f1b745f8291a630bd
Tony Bonadio <fbonadio@email.sc.edu>
2020-04-05 16:00:48 -0400

    added drawer to rest of pages, closes #94

--- CleaningHouse-master/app/src/main/java/com/example/researchapp/PropertiesScreen.java
+++ CleaningHouse-master/app/src/main/java/com/example/researchapp/PropertiesScreen.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -13,11 +14,18 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.widget.Button;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class PropertiesScreen extends AppCompatActivity implements View.OnClickListener {
@@ -31,6 +39,8 @@ public class PropertiesScreen extends AppCompatActivity implements View.OnClickL
     private DatabaseReference myRef;
     Button uploadNewPropertyButton;
     Button displayPropertyButton;
+    DrawerLayout drawer;
+    NavigationView navigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,6 +51,15 @@ public class PropertiesScreen extends AppCompatActivity implements View.OnClickL
         propText = (TextView) findViewById(R.id.Property);
         propNickname = (EditText) findViewById(R.id.propertyNickname);
         setSupportActionBar(toolbar);
+
+        drawer = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+        setupDrawerContent(navigationView);
         uploadNewPropertyButton.setOnClickListener(this);
         displayPropertyButton.setOnClickListener(this);
         mFirebaseAuth = FirebaseAuth.getInstance();
@@ -86,7 +105,71 @@ public class PropertiesScreen extends AppCompatActivity implements View.OnClickL
             }
         });
     }
+    private void setupDrawerContent(NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        selectDrawerItem(menuItem);
+                        return true;
+                    }
+                });
+    }
 
+    public void selectDrawerItem(MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+            case R.id.nav_profile:
+                Intent h = new Intent(this, Profile.class);
+                startActivity(h);
+                break;
+            case R.id.nav_schedule:
+                Intent i = new Intent(this, ScheduleScreen.class);
+                startActivity(i);
+                break;
+            case R.id.property:
+                Intent j = new Intent(this, PropertiesScreen.class);
+                startActivity(j);
+                break;
+            case R.id.listings:
+
+                break;
+            case R.id.nav_message:
+                Intent l = new Intent(this, MessageScreen.class);
+                startActivity(l);
+                break;
+            case R.id.transactions:
+                Intent m = new Intent(this, BillingScreen.class);
+                startActivity(m);
+                break;
+            case R.id.contacts:
+
+                break;
+            case R.id.termsOfService:
+                Intent o = new Intent(this, terms_and_conditions_page.class);
+                startActivity(o);
+                break;
+            default:
+
+        }
+        LayoutInflater inflater = getLayoutInflater();
+        LinearLayout container = (LinearLayout) findViewById(R.id.content_frame);
+        inflater.inflate(R.layout.activity_home_screen_host, container);
+        // Highlight the selected item has been done by NavigationView
+        menuItem.setChecked(true);
+        // Set action bar title
+        setTitle(menuItem.getTitle());
+        // Close the navigation drawer
+        drawer.closeDrawers();
+    }
+    public void onBackPressed(){
+        if (drawer.isDrawerOpen(GravityCompat.START)){
+            drawer.closeDrawer(GravityCompat.START);
+        }
+        else
+        {
+            super.onBackPressed();
+        }
+    }
   /*  protected void onStart() {
         super.onStart();
         mFirebaseAuth.addAuthStateListener(mAuthStateListener);
--- CleaningHouse-master/app/src/main/java/com/example/researchapp/terms_and_conditions_page.java
+++ CleaningHouse-master/app/src/main/java/com/example/researchapp/terms_and_conditions_page.java
@@ -1,17 +1,27 @@
 package com.example.researchapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.LinearLayout;
 
 public class terms_and_conditions_page extends AppCompatActivity {
 
+    DrawerLayout drawer;
+    NavigationView navigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,14 +29,80 @@ public class terms_and_conditions_page extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        drawer = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+        setupDrawerContent(navigationView);
+
+    }
+    private void setupDrawerContent(NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        selectDrawerItem(menuItem);
+                        return true;
+                    }
+                });
+    }
+
+    public void selectDrawerItem(MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+            case R.id.nav_profile:
+                Intent h = new Intent(this, Profile.class);
+                startActivity(h);
+                break;
+            case R.id.nav_schedule:
+                Intent i = new Intent(this, ScheduleScreen.class);
+                startActivity(i);
+                break;
+            case R.id.property:
+                Intent j = new Intent(this, PropertiesScreen.class);
+                startActivity(j);
+                break;
+            case R.id.listings:
+
+                break;
+            case R.id.nav_message:
+                Intent l = new Intent(this, MessageScreen.class);
+                startActivity(l);
+                break;
+            case R.id.transactions:
+                Intent m = new Intent(this, BillingScreen.class);
+                startActivity(m);
+                break;
+            case R.id.contacts:
+
+                break;
+            case R.id.termsOfService:
+                Intent o = new Intent(this, terms_and_conditions_page.class);
+                startActivity(o);
+                break;
+            default:
+
+        }
+        LayoutInflater inflater = getLayoutInflater();
+        LinearLayout container = (LinearLayout) findViewById(R.id.content_frame);
+        inflater.inflate(R.layout.activity_home_screen_host, container);
+        // Highlight the selected item has been done by NavigationView
+        menuItem.setChecked(true);
+        // Set action bar title
+        setTitle(menuItem.getTitle());
+        // Close the navigation drawer
+        drawer.closeDrawers();
+    }
+    public void onBackPressed(){
+        if (drawer.isDrawerOpen(GravityCompat.START)){
+            drawer.closeDrawer(GravityCompat.START);
+        }
+        else
+        {
+            super.onBackPressed();
+        }
     }
 
 }
