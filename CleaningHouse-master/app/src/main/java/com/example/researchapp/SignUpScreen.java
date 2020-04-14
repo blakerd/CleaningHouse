a10a3197625d6cf3fe0318e578936956f427099e
vicbot87 <vicbot87@gmail.com>
2020-04-14 13:58:14 -0400

    Working on uploading profile pics

--- CleaningHouse-master/app/src/main/java/com/example/researchapp/SignUpScreen.java
+++ CleaningHouse-master/app/src/main/java/com/example/researchapp/SignUpScreen.java
@@ -4,9 +4,13 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.provider.OpenableColumns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.content.Context;
+import android.database.Cursor;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 
@@ -17,6 +21,7 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.*;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -41,6 +46,7 @@ public class SignUpScreen extends AppCompatActivity implements View.OnClickListe
     private FirebaseAuth.AuthStateListener mAuthStateListener;
     private FirebaseDatabase mFirebaseDatabase;
     private DatabaseReference reference;
+    private StorageReference mStorageRef;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -53,6 +59,8 @@ public class SignUpScreen extends AppCompatActivity implements View.OnClickListe
         mFirebaseDatabase = FirebaseDatabase.getInstance();
         reference = mFirebaseDatabase.getReference();
 
+        mStorageRef = FirebaseStorage.getInstance().getReference();
+
 
         fullName =  (EditText) findViewById(R.id.name);
         location = (EditText) findViewById(R.id.cityState);
@@ -60,9 +68,17 @@ public class SignUpScreen extends AppCompatActivity implements View.OnClickListe
         makeAccBtn = (Button) findViewById(R.id.makeAccBtn);
 
 
-
-        profilePic.setOnClickListener(this);
         makeAccBtn.setOnClickListener(this);
+
+        profilePic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent();
+                i.setType("image/*");
+                i.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(i, "Select Image"), 101);
+            }
+        });
     }
 
 
@@ -71,6 +87,8 @@ public class SignUpScreen extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
+            switch(v.getId()) {
+                case R.id.makeAccBtn:
                 fName = fullName.getText().toString();
                 loc = location.getText().toString();
                 reference.child("Users").child(userID).child("Full Name").setValue(fName);
@@ -78,16 +96,62 @@ public class SignUpScreen extends AppCompatActivity implements View.OnClickListe
                 //yet to implement picture storage
                 Intent i = new Intent(SignUpScreen.this, HomeScreenHost.class);
                 startActivity(i);
-
+                break;
+            }
 
     }
 
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode == IMAGE && resultCode == RESULT_OK && data !=null) {
-            Uri selectedImage = data.getData();
-            profilePic.setImageURI(selectedImage);
+        if(requestCode == 101 && resultCode == RESULT_OK && data !=null) {
+            Uri uri = data.getData();
+            String path = getURIPath(this, uri);
+            String selectedImage = getFileName(uri);
+           // profilePic.setImageURI(selectedImage);
         }
+    }
+
+    private String getFileName(Uri uri)
+    {
+        String result = null;
+        if(uri.getScheme().equals("content"))
+        {
+            Cursor cursor = getContentResolver().query(uri, null, null, null, null);
+            try {
+                    if(cursor != null && cursor.moveToFirst())
+                    {
+                        result = cursor.getString(cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME));
+                    }
+            }
+            finally {
+                cursor.close();
+            }
+        }
+        if(result == null)
+        {
+            result = uri.getPath();
+            int cut = result.lastIndexOf('/');
+            if(cut != -1)
+            {
+                result = result.substring(cut + 1);
+            }
+        }
+        return result;
+    }
+
+    private String getURIPath(Context context, Uri uri)
+    {
+        String[] proj = {MediaStore.Images.Media.DATA};
+        Cursor cursor = context.getContentResolver().query(uri, proj, null, null, null);
+
+        if(cursor != null)
+        {
+            int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            cursor.moveToFirst();
+            return cursor.getString(column_index);
+
+        }
+        return null;
     }
 
 }
