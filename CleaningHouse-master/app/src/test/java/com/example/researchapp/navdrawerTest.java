0506fc3df87a6945eeefe8b7cea97a788807fbc4
Tony Bonadio <fbonadio@email.sc.edu>
2020-04-28 01:47:29 -0400

    home screen testing

--- CleaningHouse-master/app/src/test/java/com/example/researchapp/navdrawerTest.java
+++ CleaningHouse-master/app/src/test/java/com/example/researchapp/navdrawerTest.java
@@ -1,15 +1,22 @@
 package com.example.researchapp;
 
 import android.content.Intent;
+import android.view.Gravity;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
+
 
 import org.junit.Test;
 
-public class navdrawerTest {
-    HomeScreen homeS;
-    @Test
-    public void clickingLogout() {
+import static org.junit.Assert.assertEquals;
 
+public class navdrawerTest {
+
+    @Test
+    public void headerView() {
+        HomeScreen home = new HomeScreen();
+        TextView expected = home.status;
+        assertEquals(null,expected);
     }
 }
