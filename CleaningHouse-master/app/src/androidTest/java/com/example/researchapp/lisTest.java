fe36bb6c682896c3c429e01f70637ef1c11a472f
Lydia Ahrens <lydiamidori@gmail.com>
2020-04-28 03:12:09 -0400

    Behavior Testing for Listings

---
+++ CleaningHouse-master/app/src/androidTest/java/com/example/researchapp/lisTest.java
@@ -0,0 +1,56 @@
+package com.example.researchapp;
+
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.filters.LargeTest;
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.runner.AndroidJUnit4;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isClickable;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.isEnabled;
+import static androidx.test.espresso.matcher.ViewMatchers.isSelected;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class lisTest {
+
+    @Rule
+    public ActivityTestRule<Listings> mActivityTestRule = new ActivityTestRule<>(Listings.class);
+
+
+    // final EditText nameEditText =
+    // (EditText) activity.findViewById(R.id.emailButton);
+    @Test
+    public void lissTest() {
+
+        onView(withId(R.id.ListingsHeader)).check(matches(isDisplayed()));
+        onView(withId(R.id.Listing1)).check(matches(isDisplayed()));
+        onView(withId(R.id.Listing2)).check(matches(isDisplayed()));
+        onView(withId(R.id.Listing3)).check(matches(isDisplayed()));
+        onView(withId(R.id.Listing4)).check(matches(isDisplayed()));
+        onView(withId(R.id.Listing5)).check(matches(isDisplayed()));
+    }
+
+
+
+}
